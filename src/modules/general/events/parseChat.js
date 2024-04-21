3b90f517f313d3ad3b1248577ee493a9f7126272
Yincard <isbestsalt@gmail.com>
2024-04-21 07:44:42 -0400

    Update

--- src/modules/general/events/parseChat.js
+++ src/modules/general/events/parseChat.js
@@ -1,60 +1,47 @@
 const Event = require('../../../../structures/Event');
 require('dotenv').config();
 
-
-class parseChat extends Event {
+class ParseChat extends Event {
 	constructor() {
 		super({
 			name: 'messageCreate',
 			once: false,
 		});
 	}
-	async execute(client, message) {
-		if (client.user.id == message.author.id) return;
 
-		const guildId = message.guild.id
-		const channelId = message.channel.id
-		const authorId = message.author.id
-		let currentDate = new Date();
+	async execute(client, message) {
+		if (client.user.id === message.author.id) return;
+
+		const { id: guildId } = message.guild;
+		const { id: channelId } = message.channel;
+		const { id: authorId } = message.author;
+		const currentDate = new Date();
 		currentDate.setDate(currentDate.getDate() - 1);
-		let today = currentDate.toISOString().split('T')[0];
-		const guildCache = await client.cache.getGuildData(guildId)
+		const today = currentDate.toISOString().split('T')[0];
+		let guildCache = await client.cache.getGuildData(guildId);
 
 		if (!guildCache) {
-			await client.cache.setGuildData(guildId, {
+			guildCache = {
 				channels: {
 					[channelId]: {
 						[today]: {
 							[authorId]: 1,
 						},
 					},
+				},
+			};
 
-				}
-			})
 		} else {
-
-			if (!guildCache.channels[channelId]) {
-				guildCache.channels[channelId] = {
-					[today]: {
-						[authorId]: 1,
-					},
-				};
-			} else {
-				guildCache.channels[channelId][today] = {
-					...(guildCache.channels[channelId][today] || {}),
-					[authorId]: (guildCache.channels[channelId][today]?.[authorId] || 0) + 1,
-				};
-			}
-
-			await client.cache.setGuildData(guildId, guildCache)
-
+			guildCache.channels[channelId] = {
+				[today]: {
+					...guildCache.channels[channelId]?.[today],
+					[authorId]: (guildCache.channels[channelId]?.[today]?.[authorId] || 0) + 1,
+				},
+			};
 		}
-		
+
+		await client.cache.setGuildData(guildId, guildCache);
 	}
 }
 
-
-
-module.exports = parseChat;
-
-
+module.exports = ParseChat;
